7aee7c352c6511a3a88374a7d085f20445cc7c64
Thomas Reese <thomas.reese@sproutsocial.com>
2023-05-01 19:32:07 +0000

    "Initial commit"

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,21 @@
+import mealImg from "../../assets/meals.jpg";
+import React from "react";
+import classes from "./Header.module.css";
+import HeaderCardButton from "./HeaderCardButton";
+
+const Header = props => {
+  return (
+    <React.Fragment>
+      <header className={classes.header}>
+        <h1>ReactMeals</h1>
+        <HeaderCardButton />
+      </header>
+
+      <div className={classes["main-image"]}>
+        <img src={mealImg} alt="A table full of delicious food!" />
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default Header;
